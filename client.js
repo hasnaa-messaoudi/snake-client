146d30e4de68d7f5a544691cb8061c69328de818
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-08-25 20:16:13 +0000

    add message that player is successflly connected and add a player name

--- client.js
+++ client.js
@@ -5,12 +5,19 @@ const connect = function() {
     host: '10.0.2.15',
     port: 50541
   });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  }); 
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+
+  conn.write('Name: HME');
   
 
   return conn;
