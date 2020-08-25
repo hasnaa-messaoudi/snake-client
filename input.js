d51727dca347b7bfb583f9873bcbafe8a92fa796
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-08-25 23:57:46 +0000

    implement WASD Movement and send them to the server

--- input.js
+++ input.js
@@ -2,19 +2,26 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
+let connection;
+
+const handleUserInput = function(key, conn) {
+  if (key === 'w') conn.write('Move: up');
+  else if (key === 's') conn.write('Move: down');
+  else if (key === 'd') conn.write('Move: right');
+  else if (key === 'a') conn.write('Move: left');
+  else if (key === '\u0003') {
     process.exit();
   }
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', (key) => {
-    handleUserInput(key);
+    handleUserInput(key, conn);
   });
   return stdin;  
 }
